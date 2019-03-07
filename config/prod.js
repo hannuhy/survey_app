1da413c8ef101468656133070a05e0a99e702125
Hannu Hyvärinen <hajatahy@student.jyu.fi>
2019-03-07 18:10:38 +0200

    update

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,3 @@
-// prod google client id: 444325709194-6a5mp13st5b4tvuirno7mibjofne7ds5.apps.googleusercontent.com
-// prod google secret: fGNCnJiJYFnAPhFWjqmACqUU
-// prod mongo uri: mongodb+srv://admin_prod:ObkRbOLomTTpLqqy@emaily-prod-a9k7t.mongodb.net/test?retryWrites=true
-
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
