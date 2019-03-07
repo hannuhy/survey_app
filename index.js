7f6b86e00c03e4108d53e598780c08ba74b69740
Hannu Hyvärinen <hajatahy@student.jyu.fi>
2019-03-07 18:06:02 +0200

    finished auth flow

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User'); // initialize user model
+require('./services/passport'); // initialize passport configuration
+
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({buy: 'buddy'});
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
